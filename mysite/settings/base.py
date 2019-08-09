59071d853a067eb06c35a05666f0f41f78a909cb
Yui <260608024g@gamil.com>
2019-08-09 11:42:30 +0800

    部署配置优化

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -13,19 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n^n!uywd09kx5yp+h9ujjp=2gjchh+cbv5rnmm-i-q&v$vij_+'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['*']
 
 
 # Application definition
